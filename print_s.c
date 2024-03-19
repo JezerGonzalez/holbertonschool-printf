03ba77236f8d5fa0d2beacdbd56b6d0730e7ee7b
JezerGonzalez <jezergonzalez@gmail.com>
2024-03-19 13:28:07 -0400

    moved to other file

---
+++ print_s.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+/**
+ *print_s - prints a string if a % is followd by an s
+ *@args: function on the structure
+ */
+void print_s(va_list *args)
+{
+	int i;
+	char *ptr = va_arg(*args, char *);
+
+	for (i = 0; ptr[i] != '\0'; i++)
+		_putchar(ptr[i]);
+}
