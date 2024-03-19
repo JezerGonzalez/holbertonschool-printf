80a508ed47797feb80b5ea26bcd5d3e1edc28d98
JezerGonzalez <jezergonzalez@gmail.com>
2024-03-19 14:02:11 -0400

    added betty documentation

--- main.h
+++ main.h
@@ -1,4 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdarg.h>
+/**
+ * struct letter - structure to find the format specifier
+ * @cmp: comparing characters to the format specifier
+ * @f: function
+ */
 typedef struct letter
 {
 char *cmp;
@@ -8,3 +15,4 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 void print_s(va_list *args);
 void print_c(va_list *args);
+#endif /* MAIN_H */
