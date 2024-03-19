4d0dcdd9274ff64c4ab8d1ac570f2695abcacb5b
JezerGonzalez <jezergonzalez@gmail.com>
2024-03-19 14:56:51 -0400

    updated NULL condition with write command

--- print_s.c
+++ print_s.c
@@ -2,6 +2,7 @@
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <unistd.h>
 /**
  *print_s - prints a string if a % is followd by an s
  *@args: function on the structure
@@ -11,7 +12,7 @@ int print_s(va_list *args)
 	int i;
 	char *ptr = va_arg(*args, char *);
 	if (ptr == NULL)
-		_putchar('0');
+		return (write(1, "(null)", 6));
 
 	for (i = 0; ptr[i] != '\0'; i++)
 		_putchar(ptr[i]);
