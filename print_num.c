ae596492a01ad8e5e36b48f7f76c48dcb1ad4929
Kevin Rentas <kevrentas3@gmailcom>
2024-03-20 12:32:17 -0400

    updating my branch with updated printf code and a new file to complement changes aded to the existing printf code

---
+++ print_num.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * print_number - Entry point
+ *@va: el integer para print
+ * Return: ...
+ */
+
+int print_num(va_list *args)
+{
+	int i, len, r, l;
+	unsigned int abs, num, numt;
+	int n = va_arg(*args, int);
+
+	len = 0;
+	i = 0;
+	r = 1;
+	l = 1;
+	if (n < 0)
+	{
+		_putchar('-');
+		len++;
+		abs = -n;
+	} else
+	{
+		abs = n;
+	}
+
+	num = abs;
+	while (num > 0)
+	{
+		num /= 10;
+		i++;
+	}
+
+	while (r < i)
+	{
+		l *= 10;
+		r++;
+	}
+	while (l >= 1)
+	{
+		numt = (abs / l) % 10;
+		_putchar(numt + '0');
+		len++;
+		l /= 10;
+	}
+	return (len);
+}
