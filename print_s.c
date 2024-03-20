58243dded0f0e96479166528daa7727c2c091800
JezerGonzalez <jezergonzalez@gmail.com>
2024-03-20 12:52:54 -0400

    fixed betty documentation

--- print_s.c
+++ print_s.c
@@ -6,11 +6,13 @@
 /**
  *print_s - prints a string if a % is followd by an s
  *@args: function on the structure
+ *Return: string
  */
 int print_s(va_list *args)
 {
 	int i;
 	char *ptr = va_arg(*args, char *);
+
 	if (ptr == NULL)
 		return (write(1, "(null)", 6));
 
