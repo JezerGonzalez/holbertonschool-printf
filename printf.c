6c2e178f2db4e5f596606522439a1aef2076dc4f
JezerGonzalez <jezergonzalez@gmail.com>
2024-03-20 14:08:50 -0400

    turning back to previous commits

--- printf.c
+++ printf.c
@@ -11,7 +11,10 @@ int _printf(const char *format, ...)
 {
 	comparison letra[] = {{"c", print_c}, {"s", print_s},
 		{"d", print_num}, {"i", print_num}, {NULL, NULL}};
-	int i = 0, j, count = 0, total = 0;
+	int i = 0;
+	int j;
+	int count = 0;
+	int total = 0;
 
 	va_list args;
 
@@ -23,14 +26,6 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
-			for (j = 0; letra[j].cmp != NULL; j++)
-			{
-				if (format[i + 1] == *letra[j].cmp)
-				{
-					i++, total += letra[j].f(args);
-					i++, count += 2;
-				}
-			}
 			if (format[i + 1] == '%')
 			{
 				_putchar('%');
@@ -38,14 +33,19 @@ int _printf(const char *format, ...)
 			}
 			else if (format[i + 1] == '\0')
 				return (-1);
-			else if (format[i + 1] != '\0')
-				_putchar(format[i]);
+			for (j = 0; letra[j].cmp != NULL; j++)
+			{
+				if (format[i + 1] == *letra[j].cmp)
+				{
+					total += letra[j].f(args);
+					i++, count += 2;
+				}
+			}
 		}
 		else
-		{
 			_putchar(format[i]);
-		}
 	}
 	va_end(args);
+
 	return (total + (i - count));
 }
